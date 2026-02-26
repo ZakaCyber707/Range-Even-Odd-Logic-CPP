66351e85b346eeccb0da30e5fd716abe35cb6fed
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-02-26 23:46:27 +0500

    Implement iterative parity check using for-loop
    " The program uses a for loop to scan through a range (0 to 10) and applies an if-else condition to determine if each number is Even or Odd based on its remainder."

---
+++ Even_Odd_Iterator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	for (int i = 0; i <= 10; i++)
+	{
+
+		if (i % 2 == 0)
+		{
+			cout << i << ":Even" << endl;
+		}
+		else
+		{
+			cout << i<<":Odd"<<endl;
+		}
+
+
+	}
+}
+
+
